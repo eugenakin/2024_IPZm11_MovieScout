b7fb2e8f0669986afe38b8e727bb13379623d887
Yevhenii N <fend3rock@gmail.com>
2024-11-23 20:31:11 +0200

    Add RetrofitClient and base API class with methods

--- app/src/main/java/com/example/moviescout/data/api/MoviesApiService.kt
+++ app/src/main/java/com/example/moviescout/data/api/MoviesApiService.kt
@@ -1,2 +1,18 @@
 package com.example.moviescout.data.api
 
+import com.example.moviescout.data.models.MoviesListResponse
+import retrofit2.http.GET
+
+interface MoviesApiService {
+    @GET("movie/now_playing")
+    suspend fun getNowPlayingMovies(): MoviesListResponse
+
+    @GET("movie/popular")
+    suspend fun getPopularMovies(): MoviesListResponse
+
+    @GET("movie/top_rated")
+    suspend fun getTopRatedMovies(): MoviesListResponse
+
+    @GET("movie/upcoming")
+    suspend fun getUpcomingMovies(): MoviesListResponse
+}
